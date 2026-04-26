fa00511842eb7c92ebd558e06746f5cf86ec0ba6
Hans <hans@ufsm.br>
2026-04-25 22:14:29 -0300

    chore: upgrade to Next.js 15, modernize config, add keywords

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/IDEAlg',
+  assetPrefix: '/IDEAlg',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
